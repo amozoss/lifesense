64d165932206907debd7d684199a1a401bf9816c
Zach Toolson <ztoolson@instructure.com>
2014-12-10 14:48:21 -0700

    fixed email message and check

--- app/controllers/api/v2/records_controller.rb
+++ app/controllers/api/v2/records_controller.rb
@@ -18,7 +18,6 @@ class Api::V2::RecordsController < ApplicationController
     sensor = pin_number.sensor if pin_number
 
     if sensor
-      binding.pry
       @record = sensor.records.build(x: DateTime.now.to_i * 1000, y: record_params["y"])
       if @record.save
         # only send email if lower is a valid number
@@ -27,11 +26,10 @@ class Api::V2::RecordsController < ApplicationController
           calculator = Dentaku::Calculator.new
           formula_value = calculator.evaluate(sensor.formula, x: @record.y)
 
+          # Send email if formula_value is below sensor lower value
           if formula_value && formula_value < sensor.lower.to_f
-            # SEND EMAIL
-  #          RecordMailer.send_record(user).deliver
+            RecordMailer.send_record(user, @record).deliver
           end
-          binding.pry
         end
       end
     end
--- app/mailers/record_mailer.rb
+++ app/mailers/record_mailer.rb
@@ -1,12 +1,13 @@
 class RecordMailer < ActionMailer::Base
 #  default from: "data@lifesense.com"
-  default :from => 'any_from_address@example.com'
+  default :from => 'no-reply@lifesense.herokuapp.com'
 
   def send_record(user, record)
     @user = user
     @record = record
 
-    mail(to: @user.email, subject: 'LifeSense Data Record')
+    mail(to: 'zach.toolson@gmail.com', subject: 'LifeSense Data Record')
+#    mail(to: @user.email, subject: 'LifeSense Data Record')
   end
 
   def hello_world(user)
